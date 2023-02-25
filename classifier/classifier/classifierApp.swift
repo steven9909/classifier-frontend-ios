767fce86094eb60f86e38b301d5588fd507b20f5
steven9909 <sshyun@edu.uwaterloo.ca>
2023-02-24 19:12:12 -0500

    initial project setup

---
+++ classifier/classifier/classifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  classifierApp.swift
+//  classifier
+//
+//  Created by Steven Hyun on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct classifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
